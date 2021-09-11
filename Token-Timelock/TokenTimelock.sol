ddda4b90038709c7f57341ceb51d6df886012708
DaminduSandaruwan <kgdsbandarak@gmail.com>
2021-09-11 17:59:01 +0530

    Token Timelock

---
+++ Token-Timelock/TokenTimelock.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.1;
+
+import "./Token.sol";
+
+contract TokenTimelock{
+    Token public token;
+    address public beneficiary;
+    uint256 public releaseTime;
+    
+    constructor(Token _token, address _benificiary, uint256 _releaseTime) public {
+        require(_releaseTime > block.timestamp);
+        token = _token;
+        beneficiary = _benificiary;
+        releaseTime = _releaseTime;
+    }
+    
+    function release() public {
+        require(block.timestamp>= releaseTime);
+        uint256 amount = token.balanceOf(address(this));
+        require(amount>0);
+        token.transfer(beneficiary, amount);
+    }
+}
